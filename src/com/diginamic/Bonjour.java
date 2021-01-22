bcb95f16f25910fa899abf82bb3f9c85c5dbf034
formation <formation@DESKTOP-SCD93KD>
2021-01-22 16:13:55 +0100

    V1

---
+++ src/com/diginamic/Bonjour.java
@@ -0,0 +1,15 @@
+package com.diginamic;
+/**
+ * 
+ * @author formation
+ *
+ */
+public class Bonjour {
+/**
+ * 	
+ * @param args
+ */
+public static void main(String[] args) {
+	System.out.println("bonjour a toutes");
+}
+}
